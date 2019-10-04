a7954c005791fbc1c8723ff8f50c89c2fe616ed9
Serge Koudoro <skab12@gmail.com>
2019-10-04 22:17:58 +0200

    simplify code

--- docs/examples/moving_arrow.py
+++ docs/examples/moving_arrow.py
@@ -3,25 +3,26 @@ import numpy as np
 
 centers = np.array([[0, 0, 0], [1, 0, 0], [2, 0, 0]])
 dirs = np.array([[0, 1, 0], [1, 0, 0], [0, 0.5, 0.5]])
+colors = np.random.rand(3, 3)
 
-arrows = actor.arrow(centers=centers, colors=(1, 0, 0), directions=dirs)
+arrows = actor.arrow(centers=centers, colors=colors, directions=dirs)
 showm = window.ShowManager(size=(800, 600))
+
+nb_pts = arrows.GetMapper().GetInput().GetReferenceCount()
+pts = utils.numpy_support.vtk_to_numpy(arrows.GetMapper().GetInput().GetPoints().GetData())
 
 
 def timer_callback(_obj, _event):
-    nb_pts = arrows.GetMapper().GetInput().GetReferenceCount()
-    pts = utils.numpy_support.vtk_to_numpy(arrows.GetMapper().GetInput().GetPoints().GetData())
+    global pts
     # centers = [np.mean(p, axis=0).astype(np.int) for p in np.split(pts, nb_pts)]
     # print(centers)
 
     # Per elements
-    elements = np.split(pts, nb_pts)
-    elements[0] += np.array([[0.01, 0, 0]])
-    elements[1] -= np.array([[0.02, 0, 0]])
-    elements[2] += np.array([[0.03, 0.03, 0]])
-    pts = np.concatenate(elements)
-
-    # print(pts)
+    centers_update = np.array([[0.01, 0, 0],
+                               [-0.02, 0, 0],
+                               [0.03, 0, 0.]])
+    centers_update = np.repeat(centers_update, pts.shape[0]/nb_pts, axis=0)
+    pts += centers_update
 
     arrows.GetMapper().GetInput().SetPoints(utils.numpy_to_vtk_points(pts))
     arrows.GetMapper().GetInput().ComputeBounds()
